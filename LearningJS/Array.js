29173024edc49eb58e9b0fa7cdb586046714dc3e
Aditya Singh <101989898+SAditya14@users.noreply.github.com>
2024-01-02 07:24:00 +0000

    strings in js

--- LearningJS/Array.js
+++ LearningJS/Array.js
@@ -3,5 +3,4 @@ const num= [1,2,3,4,5,6]
 console.log(num)
 
 const Heroes= {"Batman" : "Numnber1"}
-console.log(Heroes.Batman
-    )
+console.log(Heroes.Batman)
---
+++ LearningJS/Strings.js
@@ -0,0 +1,21 @@
+// // let bankBalance = "1000"   //this is string
+// // let bankBalance1 = parseInt(bankBalance) // for round off number
+// // //to get which datatype it is we can use
+// // console.log(typeof bankBalance1)
+// // console.log(bankBalance1)
+
+
+// // ConCatenantion
+// // const firstName = "Aditya"
+// // const lastName = "Singh"
+// // We are having two ways to concatenante
+// // console.log(firstName + " " + lastName)  -->//first way, this is not that efficient way
+// // console.log(`First name is ${firstName} and last name is ${lastName} `) --> 2nd way but efficient way 
+
+// //Accessing values like array in string
+// const username= String("AKS")
+// // console.log(username[1])
+// //there are so many functions are there for string few of them are like.
+// console.log(username.length)
+// console.log(username.charAt(2))
+
